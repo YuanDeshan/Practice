0a243ee4ee6a7ef41a722ea28ce58898b29602a2
YuanDeshan <1737671795@qq.com>
2019-04-20 19:58:02 +0800

    memcpy

---
+++ memcpy.c
@@ -0,0 +1,23 @@
+﻿#include<stdio.h> 
+#include <assert.h>
+#include <string.h>
+void* Memcpy (void* dest,const void* src,size_t count){
+	void* ret = dest;
+	assert(dest);
+	assert(src);
+	while (--count){
+		*(char*)dest = *(char*)src;
+			dest = (char*)dest + 1;
+			src = (char*)src + 1;
+	}
+	return ret;
+}
+
+int main(){
+	char arr1[1024] = "";
+	const char arr2[1024] = "ABCDEF";
+	Memcpy(arr1, arr2,strlen(arr2)+1);
+	printf("%s\n", arr1);
+	return 0;
+}
+
