fcfcb9b59bc5d483c4950de755b6519f383174a1
YuanDeshan <1737671795@qq.com>
2019-05-20 20:45:55 +0800

    栈

---
+++ stack.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <assert.h>
+#include <malloc.h>
+
+typedef int STDatatype;
+
+typedef struct Stack{
+	STDatatype* arr;
+	int top;//Õ»¶¥
+	int capacity;//ÈÝÁ¿
+}Stack;
+
+//
+void StackInit(Stack* ps){
+
+}
+
+void StackPush(Stack* ps,STDatatype data){
+
+}
+
+void StackPop(Stack* ps){
+
+}
+
+STDatatype StackTop(Stack* ps){
+
+}
+
+int StackSize(Stack* ps){
+
+}
+
+int StackEmpty(Stack* ps){
+
+}
+
+void StackDestroy(Stack* ps){
+
+}
+
+void StackTest(){
+
+
+}
+
+int main(){
+
+	return 0;
+}
