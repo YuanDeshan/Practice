0877b3c1eefa1dcaeb29aa2ff97b958300e4ef05
YuanDeshan <1737671795@qq.com>
2019-07-11 19:19:48 +0800

    zs

--- aaaa.c
+++ aaaa.c
@@ -4,10 +4,11 @@ int mainz(){
 	extern int A, B, C;
 	printf("Please enter three integer numbers:");
 	scanf("%d%d%d",&A,&B,&C);
-	printf("max is %d/n", max());
+	printf("max is %d", max());
 	return 0;
 }
 int A, B, C;
+
 int max(){
 	int m;
 	m = A > B ? A : B;
