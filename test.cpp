e15d1949e589cb77b4206949834b0e642a189b44
YuanDeshan <1737671795@qq.com>
2019-07-21 17:51:55 +0800

    zs

---
+++ test.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int fun(int month){
+	if (month<3){
+		return 1;
+	}
+	else{
+		int first = 1;
+		int second = 1;
+		int count;
+		while (month>2){
+			count = first + second;
+			first = second;
+			second = count;
+			--month;
+		}
+		return count;
+	}
+}
+
+int  main(){
+	int month;
+	cin >> month;
+	cout << fun(month) << endl;
+	return 0;
+}
